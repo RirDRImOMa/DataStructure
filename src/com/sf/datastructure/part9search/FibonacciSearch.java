2741a8ba98af7794f6a5002073bd739222c5911b
RirDRImOMa <bianbo673894770@126.com>
2021-04-05 20:53:44 +0800

    斐波那契查找法

---
+++ src/com/sf/datastructure/part9search/FibonacciSearch.java
@@ -0,0 +1,12 @@
+package com.sf.datastructure.part9search;
+
+/**
+ * Created by 80002946 on 2018/4/29.
+ * 斐波那契查找法
+ *
+ * 1.斐波那契查找法会少于二分查找法
+ * 2.需要二位生成斐波那契树
+ */
+public class FibonacciSearch {
+
+}
